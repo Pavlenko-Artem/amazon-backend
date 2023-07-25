fd18c08c589204cda2285f0a74ae826e45830dfb
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-07-25 15:02:21 +0300

    Добавлен метод получения пользователя по id

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { returnUserObject } from './return-user.object'
+import { Prisma } from '@prisma/client'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(private prismaService: PrismaService) {}
+
+	async byId(id: number, selectObject: Prisma.UserSelect = {}) {
+		const user = await this.prismaService.user.findUnique({
+			where: { id },
+			select: {
+				...returnUserObject,
+				favorites: {
+					select: {
+						id: true,
+						name: true,
+						price: true,
+						images: true,
+						slug: true
+					}
+				},
+				...selectObject
+			}
+		})
+
+		if (!user) {
+			throw new Error('Пользователь не найден')
+		}
+
+		return user
+	}
+}
