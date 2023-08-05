2136be9021767fe1f6a6bb52166798860caec5ee
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-08-05 16:32:19 +0300

    Добавлен логгер http

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as morgan from 'morgan'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.setGlobalPrefix('api')
 	app.enableCors()
+	app.use(morgan('dev'))
 	await app.listen(5000)
 }
 bootstrap()
