148df2e82d20a2bf3a3e0847fb1fb5ab12beac7b
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-08-05 16:58:46 +0300

    Добавлено сообщение если введён неправильный email

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsString, MinLength } from 'class-validator'
 
 export class AuthDto {
-	@IsEmail()
+	@IsEmail({}, { message: 'Неверно указан Email' })
 	email: string
 
 	@MinLength(6, {
