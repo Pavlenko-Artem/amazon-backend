cf897bdc095d68b56bbf23461ba4675eaefbf029
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-08-12 16:36:42 +0300

    Изменены параметры стратегии

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,24 @@ import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module'
 import { ProductModule } from './product/product.module'
-import { ReviewModule } from './review/review.module';
-import { CategoryModule } from './category/category.module';
-import { OrderModule } from './order/order.module';
-import { StatisticsModule } from './statistics/statistics.module';
-import { PaginationModule } from './pagination/pagination.module';
+import { ReviewModule } from './review/review.module'
+import { CategoryModule } from './category/category.module'
+import { OrderModule } from './order/order.module'
+import { StatisticsModule } from './statistics/statistics.module'
+import { PaginationModule } from './pagination/pagination.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProductModule, ReviewModule, CategoryModule, OrderModule, StatisticsModule, PaginationModule],
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		ProductModule,
+		ReviewModule,
+		CategoryModule,
+		OrderModule,
+		StatisticsModule,
+		PaginationModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
