4aef47a0e99fcd4ef5c1e40d2a2e81edb2bb3ba3
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-08-26 15:21:11 +0300

    Добавлены контроллеры категорий

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,62 @@
-import { Controller } from '@nestjs/common';
-import { CategoryService } from './category.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { CategoryService } from './category.service'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CategoryDto } from './category.dto'
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+	constructor(private readonly categoryService: CategoryService) {}
+
+	@Get()
+	async getAll() {
+		return this.categoryService.getAll()
+	}
+
+	@Get(':slug')
+	async getBySlug(@Param('slug') slug: string) {
+		return this.categoryService.bySlug(slug)
+	}
+
+	@Get(':categoryId')
+	@Auth()
+	async getById(@Param('categoryId') categoryId: string) {
+		return this.categoryService.byId(+categoryId)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Post()
+	async create() {
+		return this.categoryService.create()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Put(':categoryId')
+	async update(
+		@Param('categoryId') categoryId: string,
+		@Body() dto: CategoryDto
+	) {
+		return this.categoryService.update(+categoryId, dto)
+	}
+
+	@HttpCode(200)
+	@Auth()
+	@Delete(':categoryId')
+	async delete(@Param('categoryId') categoryId: string) {
+		return this.categoryService.detele(+categoryId)
+	}
 }
