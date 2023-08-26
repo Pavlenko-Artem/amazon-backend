0ed5a0fe4a2ede603f4f04d1ad9af2cb2310476d
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-08-26 13:51:15 +0300

    Добавлены методы в category.service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { faker } from '@faker-js/faker'
+import { PrismaService } from 'src/prisma.service'
+import { returnCategoryObject } from './return-category.object'
+import { CategoryDto } from './category.dto'
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+	constructor(private prismaService: PrismaService) {}
+
+	async byId(id: number) {
+		const category = await this.prismaService.category.findUnique({
+			where: { id },
+			select: returnCategoryObject
+		})
+
+		if (!category) {
+			throw new NotFoundException('Категория не найдена')
+		}
+
+		return category
+	}
+
+	async bySlug(slug: string) {
+		const category = await this.prismaService.category.findUnique({
+			where: { slug },
+			select: returnCategoryObject
+		})
+
+		if (!category) {
+			throw new NotFoundException('Категория не найдена')
+		}
+
+		return category
+	}
+
+	async getAll() {
+		return this.prismaService.category.findMany({
+			select: returnCategoryObject
+		})
+	}
+
+	async create() {
+		return this.prismaService.category.create({
+			data: {
+				name: '',
+				slug: ''
+			}
+		})
+	}
+
+	async update(id: number, dto: CategoryDto) {
+		return this.prismaService.category.update({
+			where: {
+				id
+			},
+			data: {
+				name: dto.name,
+				slug: faker.helpers.slugify(dto.name)
+			}
+		})
+	}
+
+	async detele(id: number) {
+		return this.prismaService.category.delete({
+			where: {
+				id
+			}
+		})
+	}
+}
